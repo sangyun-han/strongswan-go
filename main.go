4b887a2d649c08163be6a7ae7da08de3981a452e
sangyun-han <sangyun628@gmail.com>
2018-05-01 18:19:15 +0900

    init godep and add an exception of ide to .gitignore file

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import ("github.com/bronze1man/goStrongswanVici")
+import (
+
+    "github.com/bronze1man/goStrongswanVici"
+    "fmt"
+)
 
 func main() {
     fmt.Println("test")
+    client, err := goStrongswanVici.NewClientConnFromDefaultSocket()
+    if err != nil {
+        panic(err)
+    }
+    defer client.Close()
+
+    v, err := client.Version()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("%#v \n", v)
 
 }
