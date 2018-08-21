20148a68a577c46b2f226d274ed57065901d4808
Sangyun Han <sangyun628@gmail.com>
2018-08-21 10:47:35 +0900

    [test] test code

--- main.go
+++ main.go
@@ -4,42 +4,62 @@ import (
     "github.com/sangyun-han/strongswan-go/vici"
     "fmt"
     "encoding/json"
-    "github.com/sangyun-han/strongswan-go/monitoring"
+
+    "reflect"
 )
 
-func main() {
-    monitoring.ExecuteTunnelMonitor()
-
-    vips := []string{"1", "2", "3"}
-
-    testSa := vici.IkeSa{
-        Uniqueid: "876",
-        Version: "1",
-        State: "ESTABLISHED",
-        Local_host: "Local_host",
-        Local_id: "Local_id",
-        Remote_host: "Remote_host",
-        Remote_id: "Remote_id",
-        Initiator: "yes",
-        Initiator_spi: "3fc123661bb52609",
-        Responder_spi: "Responder_spi",
-        Encr_alg: "Encr_alg",
-        Encr_keysize: "Encr_keysize",
-        Integ_alg: "Integ_alg",
-        Integ_keysize: "Integ_keysize",
-        Prf_alg: "Prf_alg",
-        Dh_group: "Dh_group",
-        Established: "Established",
-        Rekey_time: "Rekey_time",
-        Reauth_time: "Reauth_time",
-        Remote_vips: vips,
-    }
-
-    fmt.Println(testSa)
-    fmt.Println()
-    fmt.Println()
-    resultSa, _ := json.Marshal(testSa)
-    fmt.Println(string(resultSa))
-
-
+type TunnelStats struct {
+    Id       string `json:"id"`
+    Version  string `json:"version"`
+    Status   string `json:"status"`
+    BytesIn  string `json:"bytes_in"`
+    BytesOut string `json:"bytes_out"`
 }
+
+func main() {
+    executeTunnelMonitor()
+}
+
+// passive mode monitor
+func executeTunnelMonitor() {
+    client, err := vici.NewViciClientFromDefaultSocket()
+    if err != nil {
+        panic(err)
+    }
+    defer client.Close()
+
+    // To be implemented
+    sasList, err := client.ListSas("", "")
+    result, err := json.Marshal(sasList)
+    fmt.Println(sasList)
+    fmt.Println()
+    fmt.Println()
+    fmt.Println(string(result))
+    fmt.Println("End of test")
+    for key, value := range sasList {
+        fmt.Println("key=", key)
+        fmt.Println("value=", value)
+        fmt.Println()
+        fmt.Println()
+        for k, v := range value {
+            fmt.Println(k, " : ", v)
+            fmt.Println(reflect.TypeOf(v))
+        }
+        //
+        //fmt.Println(value["pdc"].Child_sas["pdc"].Bytes_in)
+        //fmt.Println()
+        //fmt.Println()
+        //fmt.Println(value["tims"])
+        //fmt.Println()
+        //fmt.Println()
+        //fmt.Println(value["pdc"].State)
+        //fmt.Println()
+        //fmt.Println()
+    }
+    fmt.Println("End of loop")
+}
+
+/*
+- IPsec tunnel monitor as a daemon mode
+- IPsec tunnel monitor as a passive mode
+ */
